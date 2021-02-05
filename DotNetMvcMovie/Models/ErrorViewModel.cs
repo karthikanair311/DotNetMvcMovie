cfbbe3188b45fedfe396cf9b5b9547d15a3b1487
Karthika Nair <karthika.rajan@foundingminds.com>
2021-02-05 19:02:15 +0530

    Add project files.

---
+++ DotNetMvcMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetMvcMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
